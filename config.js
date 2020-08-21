3669a5b3c19c8b9edc346bf790d6d528f826a259
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-08-21 21:15:12 +0300

    refactor: config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_ATLAS_DB_CONNECTION_STRING:
+    "mongodb+srv://benyam-7:oqTkDRmuPTdmGH6Q@cluster-kdame-gebeya-0.swdy5.mongodb.net/KdameGebeya?retryWrites=true&w=majority",
+};
