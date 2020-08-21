518cdf53f210d6474e89ba883a02e8791944c4bd
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-08-21 20:36:23 +0300

    fix: fix port to PORT

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const { MONGO_ATLAS_DB_CONNECTION_STRING } = require("./config");
 
 // PORT
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const typeDefs = gql`
   type Query {
