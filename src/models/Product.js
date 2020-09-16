451ceba1aabaad40de21c70241b4dfbaf9296e37
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-09-16 12:28:04 +0300

    feat: add create product mutation and get all  products query

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,8 +1,29 @@
 const { model, Schema } = require('mongoose');
+
 const productSchema = new Schema({
-  type: String,
-  name: String,
-  userId: String,
+  productName: String,
+  uniqueAttributes: {
+    geographicalDesignation: { type: String },
+    grade: { type: String },
+    group: { type: String },
+    uniqueName: { type: String },
+  },
+  user: {
+    email: String,
+    role: String,
+
+    company: {
+      websiteUrl: { type: String },
+      companyName: { type: String },
+      companyEmail: { type: String },
+      address: {
+        country: { type: String },
+        city: { type: String },
+        street: { type: String },
+        postalCode: { type: String },
+      },
+    },
+  },
 });
 
 module.exports = model('Product', productSchema);
--- src/schema/product.js
+++ src/schema/product.js
@@ -2,13 +2,38 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
   extend type Mutation {
-    postProduct(type: String!, name: String!): Product!
+    postProduct(
+      product: ProductInput!
+      company: CompanyInput!
+    ): Product!
     deleteProductPost(id: ID!): String!
+  }
+  extend type Query {
+    products: [Product!]
   }
   type Product {
     id: ID!
-    name: String!
-    type: String!
-    userId: String!
+    user: User!
+    productName: String!
+    uniqueAttributes: ProductUniqueAttributes!
+  }
+
+  type ProductUniqueAttributes {
+    geographicalDesignation: String
+    grade: String
+    group: String
+    uniqueName: String
+  }
+
+  input ProductInput {
+    productName: String!
+    uniqueAttributes: ProductUniqueAttributesInput!
+  }
+
+  input ProductUniqueAttributesInput {
+    geographicalDesignation: String
+    grade: String
+    group: String
+    uniqueName: String
   }
 `;
